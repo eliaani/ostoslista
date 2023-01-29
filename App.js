5fd8e42896cc2e435e039aebad18a102c48f50c1
Elias <eliashelin98@gmail.com>
2023-01-29 15:52:05 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {SafeAreaView, View, StyleSheet, Text, TextInput, FlatList} from 'react-native';
+import { Button } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [tuote, setTuote] = React.useState();
+  const [lista, setLista] = React.useState([]);
+
+  const buttonPressed = () => {
+    const temp = {id: tuote, title: tuote}
+    setLista([...lista, temp])
+  }
+  const buttonPressed2 = () => {
+    setLista([])
+  }
+  console.log(lista)
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TextInput
+        style={styles.input}
+        onChangeText={setTuote}
+        value={tuote}
+        placeholder="Tuote"
+      />
+      <View style={styles.buttoncontainer}>
+        <View style={styles.button}>
+          <Button onPress={buttonPressed} title="ADD"/>
+        </View>
+        <View style={styles.button}>
+          <Button onPress={buttonPressed2} title="CLEAR" />
+        </View>
+      </View>
+      <Text style={styles.title}>Shopping List</Text>
+      <FlatList 
+        data={lista}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <Text>{item.title}</Text>
+        )}
+        />
+    </SafeAreaView>
+
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  input: {
+    height: 40,
+    width: 200,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
+  buttoncontainer: {
+    padding: 16,
+    gap: 10,
+    justifyContent: 'center',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  container: {
+    padding: 16,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  button:{
+    marginHorizontal: 10,
+  },
+  title:{
+    color:"blue",
+    fontWeight:"bold"
+  }
 });
+
+export default App;
